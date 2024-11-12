e909fee43146ae281ddc6b6e57cde223c12ea5ea
Sirojbekblank <sirojbekblank@gmail.com>
2024-11-12 13:39:53 +0500

    Update problem4.cpp

--- problem4.cpp
+++ problem4.cpp
@@ -16,11 +16,19 @@ int main() {
         v.push_back(empty);
         t++;
     }
-    for(int i=0;i<t;i++) {
+    if(v[1]=='0') {
+        v[0]='1';
+        sum++;
+    }
+    for(int i=1;i<t-1;i++) {
         if(v[i-1]=='0' && '0'==v[i+1]) {
             v[i]='1';
             sum++;
         }
+    }
+    if(v[t-1]=='0') {
+        v[t]='1';
+        sum++;
     }
     if(sum>=n) {
         cout<<"YES";
