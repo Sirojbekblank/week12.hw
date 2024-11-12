8b0e97ef7ea4e58c455fd674905bf3ba5d724b91
Sirojbekblank <sirojbekblank@gmail.com>
2024-11-12 15:05:05 +0500

    Update problem4.cpp

--- problem4.cpp
+++ problem4.cpp
@@ -8,25 +8,25 @@ using namespace std;
 int main() {
     int n;
     cin>>n;
-    int sum=0,t=0;
+    int sum=0;
     char empty;
     vector<char> v;
     while(cin>>empty && empty!='!') {
         v.push_back(empty);
-        t++;
     }
+    int t =v.size();
     if(v[1]=='0' && v[0]=='0') {
         v[0]='1';
         sum++;
     }
-    for(int i=1;i<t-2;i++) {
+    for(int i=1;i<t-1;i++) {
         if(v[i-1]=='0' && v[i]=='0' && '0'==v[i+1]) {
             v[i]='1';
             sum++;
         }
     }
     if(v[t-2]=='0' && v[t-1]=='0') {
-        v[t]='1';
+        v[t-1]='1';
         sum++;
     }
     if(sum>=n) {
